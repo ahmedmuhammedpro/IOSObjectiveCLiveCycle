13104d5959454bcc5cfa9687acf418907ec8b3e1
ahmedpro <ahmedmuhammedpro@gmail.com>
2020-03-15 21:16:37 +0200

    Initial Commit

---
+++ IOSObjectiveCLiveCycle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IOSObjectiveCLiveCycle
+//
+//  Created by ahmedpro on 3/15/20.
+//  Copyright © 2020 ahmedpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
